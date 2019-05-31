af8506042cddf1d5e9816f4ca53c978c9ca3f6d2
ccysfn <jessecao911@gmail.com>
2019-05-31 16:12:00 +0800

    bug

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+         'security/ir.model.access.csv',
         'views/parents.xml',
         'views/teacher_view.xml',
         'views/templates.xml',
