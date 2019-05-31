72427af81252acbecb2f8ae535dd2a6c634f1e52
ccysfn <jessecao911@gmail.com>
2019-05-31 22:30:24 +0800

    teacher

--- models/teacher.py
+++ models/teacher.py
@@ -4,5 +4,7 @@ from odoo import models, fields, api
 
 class teacher(models.Model):
     _name = 'jyschool.teacher'
+    _inherits = {'res.partner': 'partner_id'}
 
-    name = fields.Char()
+    name = fields.Char(related='partner_id.name',string='代号', inherited=True)
+    
