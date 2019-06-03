88c51eab1380b0d21252fb7f0d7c08629bf271ef
ccysfn <jessecao911@gmail.com>
2019-06-03 10:20:33 +0800

    ds

--- models/teacher.py
+++ models/teacher.py
@@ -4,6 +4,6 @@ from odoo import models, fields, api
 
 class teacher(models.Model):
     _name = 'jyschool.teacher'
-    _inherits = {'res.partner'}
+    _inherits = {'res.partner','res.partner.id'}
 
     sub = fields.Many2one('jyschool.subject',string='特长学科')
