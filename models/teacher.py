4c6332c2423ad56cea37a5af974a74db8c6eded4
ccysfn <jessecao911@gmail.com>
2019-06-03 09:27:41 +0800

    aa

--- models/teacher.py
+++ models/teacher.py
@@ -6,5 +6,5 @@ class teacher(models.Model):
     _name = 'jyschool.teacher'
     _inherits = {'res.partner': 'partner_id'}
 
-    name = fields.Char(related='partner_id.name',string='代号', inherited=True)
+    name = fields.Char(related='partner_id.name',string='姓名', inherited=True)
     sub = fields.Many2one('jyschool.subject',string='特长学科')
